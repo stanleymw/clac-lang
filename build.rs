83b0effa7e265b364c7a00f774a1a597663ed573
stanleymw <107821509+stanleymw@users.noreply.github.com>
2026-04-04 20:52:51 -0400

    remove vergen stuff

--- build.rs
+++
@@ -1,8 +0,0 @@
-use vergen_git2::Emitter;
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let git2 = vergen_git2::Git2Builder::all_git()?;
-
-    Emitter::default().add_instructions(&git2)?.emit()?;
-    Ok(())
-}
